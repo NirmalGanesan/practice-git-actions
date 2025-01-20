a517bfe7004faa3de15b9c9645685f5a89a52620
Nirmal <pkdnirmal@gmail.com>
2025-01-20 13:37:21 -0600

    Created a new java project

---
+++ CalculatorApp/src/main/java/com/example/calculator/App.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+
+public class App 
+{
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static void main( String[] args )
+    {
+        int result = add(9,8);
+        System.out.println("Result: " + result);
+    }
+}
