e9b45e0f4d063f1680b4b943a15bba85457a702d
Nirmal <pkdnirmal@gmail.com>
2025-01-24 12:18:17 -0600

    Added mul functionality

--- CalculatorApp/src/main/java/com/example/calculator/App.java
+++ CalculatorApp/src/main/java/com/example/calculator/App.java
@@ -9,6 +9,9 @@ public class App
     public static int sub(int a, int b){
         return a-b;
     }
+    public static int mul(int a, int b){
+        return a*b;
+    }
     public static void main( String[] args )
     {
         int result = add(9,8);
