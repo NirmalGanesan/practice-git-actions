e577da660fe1d0c9ed94409d73fa6b5722e47bad
Nirmal <pkdnirmal@gmail.com>
2025-01-24 14:28:18 -0600

    Added div functionality

--- CalculatorApp/src/main/java/com/example/calculator/App.java
+++ CalculatorApp/src/main/java/com/example/calculator/App.java
@@ -12,6 +12,9 @@ public class App
     public static int mul(int a, int b){
         return a*b;
     }
+    public static int div(int a, int b){
+        return a/b;
+    }
     public static void main( String[] args )
     {
         int result = add(9,8);
