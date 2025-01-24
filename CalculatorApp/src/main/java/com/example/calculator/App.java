1b16f2c1ae8efc17bd8157859c3039a068f6bcb6
Nirmal <pkdnirmal@gmail.com>
2025-01-24 10:50:52 -0600

    Added a sub function as new feature

--- CalculatorApp/src/main/java/com/example/calculator/App.java
+++ CalculatorApp/src/main/java/com/example/calculator/App.java
@@ -6,6 +6,9 @@ public class App
     public static int add(int a, int b){
         return a+b;
     }
+    public static int sub(int a, int b){
+        return a-b;
+    }
     public static void main( String[] args )
     {
         int result = add(9,8);
